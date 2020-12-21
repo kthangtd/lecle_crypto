539209846862c64c5a4257ca1141f221aad44891
kthangtd <kthangtd@lecle.co.kr>
2020-12-21 20:53:39 +0700

    vers 0.0.1

--- lib/lecle_crypto.dart
+++ lib/lecle_crypto.dart
@@ -3,7 +3,7 @@ library lecle_crypto;
 import 'package:flutter/material.dart' show required;
 import 'package:encrypt/encrypt.dart';
 
-part 'crypto_srv.dart';
+part 'src/crypto_srv.dart';
 
 Future initCryptoSrv({@required List<int> key}) async {
   assert(key != null && key.length > 32);
